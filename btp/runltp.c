9ed7a04303364ebf249569a9591d77f8a9c82f52
Shi Lei <shi_lei@massclouds.com>
2024-08-29 08:15:51 +0000

    fix runltp and support '#' comment

--- btp/runltp.c
+++ btp/runltp.c
@@ -38,7 +38,7 @@ int main()
 }
 
 void test(const char *name, struct results *r) {
-    if (name == NULL || strlen(name) == 0)
+    if (name == NULL || strlen(name) == 0 || name[0] == '#')
         return;
 
     printf("[%s] ...\n", name);
