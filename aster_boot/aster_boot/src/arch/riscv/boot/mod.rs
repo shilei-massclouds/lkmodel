99fdd5af16bf6e075596153e2f63e909709d738e
Shi Lei <shi_lei@massclouds.com>
2024-12-08 10:09:47 +0000

    fix aster_boot

--- aster_boot/aster_boot/src/arch/riscv/boot/mod.rs
+++ aster_boot/aster_boot/src/arch/riscv/boot/mod.rs
@@ -7,25 +7,7 @@
 //use alloc::{string::String, vec::Vec};
 use core::arch::global_asm;
 
-use fdt::Fdt;
-use spin::Once;
-
-use crate::{
-    /*
-    boot::{
-        kcmdline::KCmdlineArg,
-        memory_region::{non_overlapping_regions_from, MemoryRegion, MemoryRegionType},
-        BootloaderAcpiArg, BootloaderFramebufferArg,
-    },
-    */
-    early_println,
-    kspace::paddr_to_vaddr,
-};
-
 global_asm!(include_str!("boot.S"));
-
-/// The Flattened Device Tree of the platform.
-pub static DEVICE_TREE: Once<Fdt> = Once::new();
 
 /*
 fn init_bootloader_name(bootloader_name: &'static Once<String>) {
@@ -102,27 +84,3 @@ fn parse_initramfs_range() -> Option<(usize, usize)> {
     Some((initrd_start, initrd_end))
 }
 */
-
-/// The entry point of the Rust code portion of Asterinas.
-#[no_mangle]
-pub extern "C" fn main(_hart_id: usize, device_tree_paddr: usize) -> ! {
-    early_println!("Enter riscv_boot");
-
-    let device_tree_ptr = paddr_to_vaddr(device_tree_paddr) as *const u8;
-    let fdt = unsafe { fdt::Fdt::from_ptr(device_tree_ptr).unwrap() };
-    DEVICE_TREE.call_once(|| fdt);
-
-    /*
-    crate::boot::register_boot_init_callbacks(
-        init_bootloader_name,
-        init_kernel_commandline,
-        init_initramfs,
-        init_acpi_arg,
-        init_framebuffer_info,
-        init_memory_regions,
-    );
-    */
-
-    //crate::boot::call_ostd_main();
-    panic!();
-}
